90fd4f0739a334aea19c67ffdc683a09a86ec97d
Hussein <hussein.osman.@student.moringaschool.com>
2025-03-18 12:28:54 +0300

    save completed changes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+// 2) welcomeMessage contains "Welcome to Flatbook, "
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// 5) excitedWelcomeMessage contains "WELCOME TO FLATBOOK, "
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// 8) shortGreeting contains "Welcome, "
+const shortGreeting = `Welcome, ${currentUser[0]}!`;  // First initial of currentUser
+
+
+
+
+
+
+
+
+
